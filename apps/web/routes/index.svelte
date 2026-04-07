bb96ddb2d435a16ac0d9675d7127793a64d11519
Filip Mårtensson <sigfrid@fiecas.com>
2026-04-07 13:13:02 +0200

    Add svelte builder

---
+++ apps/web/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import {onMount} from "svelte"
+
+    onMount(()=>console.log("hi"))
+</script>
+
+<h1>Welcome to Yedo's World</h1>
+<p>Site still under construction</p>
+ 
---
+++ apps/web/routes/thing/index.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to Yedo's Things</h1>
+<p>Site still under construction</p>
+ 
