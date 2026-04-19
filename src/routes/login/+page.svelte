1a2c597ca5c63a4d424cca2508458e7253aa0729
Filip Mårtensson <sigfrid@fiecas.com>
2026-04-19 22:27:47 +0200

    Add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,10 @@
+<form action="https://indielogin.com/authorize" method="get">
+	<label for="url">Web Address:</label>
+	<input id="url" type="text" name="me" placeholder="yourdomain.com" />
+	<p><button type="submit">Sign In</button></p>
+	<input type="hidden" name="client_id" value="https://www.yedos.world/" />
+	<input type="hidden" name="redirect_uri" value="https://www.yedos.world/login/redirect" />
+	<input type="hidden" name="state" value="7c36c16d7fc214b8750cbe36" />
+	<input type="hidden" name="code_challenge" value="q5E7lSDpLPifZ1KlPTvPm8IMkzhSwCIthamepmyaswc" />
+	<input type="hidden" name="code_challenge_method" value="S256" />
+</form>
