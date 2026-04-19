a28d210f7cc1ba6b8a2b24f62bc1ba6d3ad1ab00
Filip Mårtensson <sigfrid@fiecas.com>
2026-04-19 20:20:33 +0200

    Make site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	const props = $props();
+</script>
+
+<svelte:head>
+	<title>yedo's world.</title>
+</svelte:head>
+
+<h1>welcome to yedo's world!</h1>
+
+<h2>blog:</h2>
+
+{#if props.posts}
+	{#each props.posts as post (post.slug)}
+		<a href={resolve('/blog/[slug]', post.slug)}>{post.title}</a>
+	{/each}
+{:else}
+	no blog posts yet.
+{/if}
